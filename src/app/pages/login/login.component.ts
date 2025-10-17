edb420a83674a0e918a552f57b60a157d5d1ab19
victorhrc372 <victorhrc372@gmail.com>
2025-10-17 21:34:40 +0000

    Add RouterModule login component for Register Button

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,13 +2,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule], 
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ],
+
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'] 
 })
