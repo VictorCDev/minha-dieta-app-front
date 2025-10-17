1e133c738a578d16eeb2cdef6fe58b625b9ba9fc
victorhrc372 <victorhrc372@gmail.com>
2025-10-17 21:10:51 +0000

    Dashboard Page Inicial

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout():void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+}
